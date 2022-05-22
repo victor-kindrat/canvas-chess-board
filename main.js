71da779c20ab8277b9f57334f8c0638c49be1b01
Victor Kindrat <kindratvictor07@gmail.com>
2022-05-22 15:07:49 +0300

    chess upd

--- main.js
+++ main.js
@@ -6,6 +6,16 @@ board.height = 500;
 
 ctx.fillRect(0, 0, 500, 500);
 ctx.clearRect(15, 15, 470, 470);
+
+function drawAnChess() {
+    ctx.fillRect(30 + (squareIndex * 58.75), 60 + (rowIndex * 58.75), 30, 5);
+    ctx.fillRect(36 + (squareIndex * 58.75), 41 + (rowIndex * 58.75), 18, 20);
+    ctx.fillRect(32.5 + (squareIndex * 58.75), 38 + (rowIndex * 58.75), 25, 5);
+    ctx.fillRect(32.5 + (squareIndex * 58.75), 25 + (rowIndex * 58.75), 5, 10);
+    ctx.fillRect(42.5 + (squareIndex * 58.75), 20 + (rowIndex * 58.75), 5, 15);
+    ctx.fillRect(52.5 + (squareIndex * 58.75), 25 + (rowIndex * 58.75), 5, 10);
+}
+
 let rowIndex = 0;
 let squareIndex = 0;
 for (let i = 0; i<8; i++) {
@@ -15,18 +25,14 @@ for (let i = 0; i<8; i++) {
             ctx.fillRect(15 + (squareIndex * 58.75), 15 + (rowIndex * 58.75), 58.75, 58.75);
             if (rowIndex <= 1 || rowIndex >= 6) {
                 ctx.fillStyle = '#ffffff';
-                ctx.fillRect(30 + (squareIndex * 58.75), 60 + (rowIndex * 58.75), 30, 5);
-                ctx.fillRect(36 + (squareIndex * 58.75), 41 + (rowIndex * 58.75), 18, 20);
-                ctx.fillRect(32.5 + (squareIndex * 58.75), 37 + (rowIndex * 58.75), 25, 5);
+                drawAnChess();
             }
         } else {
             ctx.fillStyle = '#E3CBA9';
             ctx.fillRect(15 + (squareIndex * 58.75), 15 + (rowIndex * 58.75), 58.75, 58.75);
             if (rowIndex <= 1 || rowIndex >= 6) {
                 ctx.fillStyle = '#343434';
-                ctx.fillRect(30 + (squareIndex * 58.75), 60 + (rowIndex * 58.75), 30, 5);
-                ctx.fillRect(36 + (squareIndex * 58.75), 41 + (rowIndex * 58.75), 18, 20);
-                ctx.fillRect(32.5 + (squareIndex * 58.75), 37 + (rowIndex * 58.75), 25, 5);
+                drawAnChess();
             }
         }
         if (squareIndex === 7) {
